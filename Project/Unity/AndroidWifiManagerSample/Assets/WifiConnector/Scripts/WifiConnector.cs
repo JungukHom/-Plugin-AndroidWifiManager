44c10ae8197c00dcb3265e181cc94fbd9a29bc68
JungukHom <developer0223@gmail.com>
2021-05-26 15:47:47 +0900

    Add | Basic projects

---
+++ Project/Unity/AndroidWifiManagerSample/Assets/WifiConnector/Scripts/WifiConnector.cs
@@ -0,0 +1,64 @@
+﻿// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+using UnityEngine.UI;
+
+// Project
+// Alias
+
+public class WifiConnector : MonoBehaviour
+{
+    #region SingleTon
+    public static WifiConnector Instance = null;
+    #endregion
+
+    private AndroidJavaClass unityPlayer;
+    private AndroidJavaObject currentActivity;
+    private AndroidJavaObject wifiConnector;
+
+    private void Awake()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        Initialize();
+#endif
+    }
+
+    private void Initialize()
+    {
+        Instance = this;
+
+        unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        wifiConnector = new AndroidJavaObject("com.fakeeyes.wificonnector.WifiConnector");
+        wifiConnector.Call("initialize", currentActivity);
+    }
+
+    public void Toast(string text)
+    {
+        wifiConnector.Call("showToast", text);
+    }
+
+    public bool SetWifiEnabled(bool enable)
+    {
+        return wifiConnector.Call<bool>("setWifiEnabled", enable);
+    }
+
+    public string[] GetWifiList()
+    {
+        return wifiConnector.Call<string[]>("getWifiList");
+    }
+
+    public bool ConnectWifi(string ssid, string password)
+    {
+        return wifiConnector.Call<bool>("connect", ssid, password);
+    }
+
+    public bool DisconnectWifi()
+    {
+        return wifiConnector.Call<bool>("disconnect");
+    }
+}
---
+++ Project/Unity/AndroidWifiManagerSample/Assets/WifiConnector/Scripts/WifiConnectorSample.cs
@@ -0,0 +1,95 @@
+﻿// System
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Unity
+using UnityEngine;
+using UnityEngine.UI;
+
+// Project
+// Alias
+
+public class WifiConnectorSample : MonoBehaviour
+{
+    public Button btn_enable_wifi;
+    public Button btn_disable_wifi;
+    public Button btn_connect_wifi;
+    public Button btn_disconnect_wifi;
+    public Button btn_show_wifi_list;
+
+    public Text txt_wifi_list;
+
+    public InputField input_wifi_ssid;
+    public InputField input_wifi_password;
+
+    public Button btn_wifi_sample_vrcms;
+    public Button btn_wifi_sample_fe_network_5g;
+
+    private void Awake()
+    {
+        AddListeners();
+    }
+
+    private void AddListeners()
+    {
+        btn_enable_wifi.onClick.AddListener(OnEnableWifiButtonPressed);
+        btn_disable_wifi.onClick.AddListener(OnDisableWifiButtonPressed);
+        btn_connect_wifi.onClick.AddListener(OnConnectWifiButtonPressed);
+        btn_disconnect_wifi.onClick.AddListener(OnDisconnectWifiButtonPressed);
+        btn_show_wifi_list.onClick.AddListener(OnShowWifiListButtonPressed);
+
+        btn_wifi_sample_vrcms.onClick.AddListener(OnSampleVrCmsButtonPressed);
+        btn_wifi_sample_fe_network_5g.onClick.AddListener(OnSampleFeNetwork5GButtonPressed);
+    }
+
+    private void OnEnableWifiButtonPressed()
+    {
+        bool result = WifiConnector.Instance.SetWifiEnabled(true);
+        if (result)
+            WifiConnector.Instance.Toast("와이파이가 활성화되었습니다");
+        else
+            WifiConnector.Instance.Toast("와이파이 활성화에 실패하였습니다");
+    }
+
+    private void OnDisableWifiButtonPressed()
+    {
+        WifiConnector.Instance.SetWifiEnabled(false);
+    }
+
+    private void OnConnectWifiButtonPressed()
+    {
+        bool result = WifiConnector.Instance.ConnectWifi(input_wifi_ssid.text.Trim(), input_wifi_password.text.Trim());
+        if (result)
+            WifiConnector.Instance.Toast("와이파이가 성공적으로 연결되었습니다");
+        else
+            WifiConnector.Instance.Toast("와이파이 연결에 실패하였습니다");
+    }
+
+    private void OnDisconnectWifiButtonPressed()
+    {
+        WifiConnector.Instance.DisconnectWifi();
+    }
+
+    private void OnShowWifiListButtonPressed()
+    {
+        string[] wifiList = WifiConnector.Instance.GetWifiList();
+        txt_wifi_list.text = "";
+        foreach (string wifi in wifiList)
+        {
+            txt_wifi_list.text += $"{wifi}\n";
+        }
+    }
+
+    private void OnSampleVrCmsButtonPressed()
+    {
+        input_wifi_ssid.text = "vrcms";
+        input_wifi_password.text = "DoGeon94";
+    }
+
+    private void OnSampleFeNetwork5GButtonPressed()
+    {
+        input_wifi_ssid.text = "FE_NETWORK_5G";
+        input_wifi_password.text = "fakeeyes!0906";
+    }
+}
